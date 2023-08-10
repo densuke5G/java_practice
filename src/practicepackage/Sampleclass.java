c0b53427c84ab93fac466786f1e45b05583c2db1
densuke5G <danie1.apcb6160@gmail.com>
2023-08-10 22:14:51 +0900

    hensuu

--- src/practicepackage/Sampleclass.java
+++ src/practicepackage/Sampleclass.java
@@ -1,7 +1,12 @@
 package practicepackage;
 
 public class Sampleclass {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
+		System.out.println("<h1>こんにちは！</h1>");
+		
+		String player = "勇者";	
+		System.out.println(player + " はレベルアップした！");
 	}
 }
