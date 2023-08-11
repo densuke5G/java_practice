aa43e864e885fd5b736884e7fb276cd5642a1128
densuke5G <danie1.apcb6160@gmail.com>
2023-08-11 22:51:31 +0900

    while sentence

---
+++ src/practicepackage/Whilebun.java
@@ -0,0 +1,22 @@
+package practicepackage;
+
+public class Whilebun {
+	public static void main(String[] args) {
+		int i = 0;
+        while (i <= 9){
+            System.out.println("ハローラーニング");
+            i = i + 1;
+        }
+        i = 0;
+        while (i <= 15) {
+            System.out.println(i);
+            i = i + 1;
+        }
+        i = 2;
+        while (i <= 10) {
+            System.out.println(i);
+            i = i + 2;
+        }
+	}
+
+}
