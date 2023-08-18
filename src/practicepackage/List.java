910ddafebb43ba3bc4a9c8e2781657d2287d848c
densuke5G <danie1.apcb6160@gmail.com>
2023-08-19 00:19:39 +0900

    List

---
+++ src/practicepackage/List.java
@@ -0,0 +1,13 @@
+package practicepackage;
+
+public class List {
+	public static void main(String[] args) {
+        String[] array = {"戦士","侍","僧侶","魔法使い"};
+        // この下で、arrayを出力してみよう
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        
+    }
+}
